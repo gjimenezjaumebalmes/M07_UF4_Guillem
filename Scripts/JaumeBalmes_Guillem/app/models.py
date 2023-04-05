13242674f5858355754080949e35dc56c0818410
FHP187 <gjimenez@jaumebalmes.net>
2023-04-05 16:06:11 +0200

    Commit push base de datos conexion

--- Scripts/JaumeBalmes_Guillem/app/models.py
+++ Scripts/JaumeBalmes_Guillem/app/models.py
@@ -1,17 +1,51 @@
+from django.shortcuts import render
+from django.views.generic.detail import DetailView
 from django.db import models
 
-
 class Profesor(models.Model):
+    id  = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    asignatura = models.CharField(max_length=100)
+    curso = models.CharField(max_length=100)
+    email = models.EmailField()
 
-    id = models.IntegerField(primary_key=True)
-    nombre = models.TextField()
-    asignatura = models.TextField()
-    email = models.TextField()
-    curso = models.CharField(max_length=50, null=True, default=None)
+    def __str__(self):
+        return self.nombre
 
 class Alumno(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    curso = models.CharField(max_length=100)
+    email = models.EmailField()
 
-    id = models.IntegerField(primary_key=True)
-    nombre = models.TextField()
-    curso = models.CharField(max_length=50, null=True, default=None)
-    email = models.TextField()
+    def __str__(self):
+        return self.nombre
+
+def homePageView(request):
+    return render(request, 'principal.html')
+
+def profesorado(request):
+    profesores = Profesor.objects.all()
+    return render(request, 'profesorado.html', {'profesores': profesores})
+
+class ProfesorDetailView(DetailView):
+    model = Profesor
+    template_name = 'profesor_detail.html'
+    pk_url_kwarg = 'pk'
+
+def alumnado(request):
+    alumnos = Alumno.objects.all()
+    return render(request, 'alumnado.html', {'alumnos': alumnos})
+
+class AlumnoDetailView(DetailView):
+    model = Alumno
+    template_name = 'alumno_detail.html'
+    pk_url_kwarg = 'pk'
+
+    def get_object(self, queryset=None):
+        alumno_id = self.kwargs.get('pk')
+        try:
+            alumno = Alumno.objects.get(pk=alumno_id)
+        except Alumno.DoesNotExist:
+            raise Http404('Alumno no encontrado')
+        return alumno
--- Scripts/JaumeBalmes_Guillem/app/views.py
+++ Scripts/JaumeBalmes_Guillem/app/views.py
@@ -1,59 +1,23 @@
-import json
-from django.http import Http404
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Profesor, Alumno
-
+from django.http import Http404
 
 def homePageView(request):
     return render(request, 'principal.html')
 
-
 def profesorado(request):
-    with open('data/datos.json') as f:
-        data = json.load(f)
-    profesorado = data['profesorado']
-    return render(request, 'profesorado.html', {'profesorado': profesorado})
+    profesores = Profesor.objects.all()
+    return render(request, 'profesorado.html', {'profesores': profesores})
 
 class ProfesorDetailView(DetailView):
     model = Profesor
     template_name = 'profesor_detail.html'
     pk_url_kwarg = 'pk'
 
-    def get_object(self, queryset=None):
-        # Comprobar si el archivo JSON existe
-        try:
-            with open('data/datos.json', 'r') as f:
-                profesores_data = json.load(f)
-        except FileNotFoundError:
-            raise Http404('Archivo no encontrado')
-
-        # Obtener el profesor con el id correspondiente
-        profesor_id = self.kwargs.get('pk')
-
-        if profesor_id is None:
-            raise Http404('Profesor no encontrado')
-
-        try:
-            profesor_data = next((p for p in profesores_data['profesorado'] if p['id'] == int(profesor_id)), None)
-        except ValueError:
-            raise Http404('Profesor no encontrado')
-
-        # Comprobar si se ha encontrado el profesor
-        if not profesor_data:
-            raise Http404('Profesor no encontrado')
-
-        # Crear una instancia del modelo Profesor con los datos del profesor encontrado
-        profesor = Profesor(**profesor_data)
-
-        return profesor
-
 def alumnado(request):
-    with open('data/datos.json') as f:
-        data = json.load(f)
-    alumnado = data['alumnado']
-    return render(request, 'alumnado.html', {'alumnado': alumnado})
-
+    alumnos = Alumno.objects.all()
+    return render(request, 'alumnado.html', {'alumnos': alumnos})
 
 class AlumnoDetailView(DetailView):
     model = Alumno
@@ -61,29 +25,9 @@ class AlumnoDetailView(DetailView):
     pk_url_kwarg = 'pk'
 
     def get_object(self, queryset=None):
-        # Comprobar si el archivo JSON existe
-        try:
-            with open('data/datos.json', 'r') as f:
-                alumnos_data = json.load(f)
-        except FileNotFoundError:
-            raise Http404('Archivo no encontrado')
-
-        # Obtener el alumno con el id correspondiente
         alumno_id = self.kwargs.get('pk')
-
-        if alumno_id is None:
-            raise Http404('Alumno no encontrado')
-
         try:
-            alumno_data = next((p for p in alumnos_data['alumnado'] if p['id'] == int(alumno_id)), None)
-        except ValueError:
+            alumno = Alumno.objects.get(pk=alumno_id)
+        except Alumno.DoesNotExist:
             raise Http404('Alumno no encontrado')
-
-        # Comprobar si se ha encontrado el alumno
-        if not alumno_data:
-            raise Http404('Alumno no encontrado')
-
-        # Crear una instancia del modelo Alumno con los datos del alumno encontrado
-        alumno = Alumno(**alumno_data)
-
         return alumno
